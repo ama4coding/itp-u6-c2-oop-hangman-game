d5693f581663f36049f9c212a71e5d7d2a65ccd3
Andre <andre4coding@gmail.com>
2018-08-04 18:47:56 +0000

    Updated guess

--- hangman/game.py
+++ hangman/game.py
@@ -22,8 +22,45 @@ class GuessAttempt(object):
 
 
 class GuessWord(object):
-    pass
+    def __init__(self, word):
+        self.answer = word
+        self.masked = '*' * len(word)
+        
+        if self.answer == '':
+            raise InvalidWordException
+        
+    def perform_attempt(self, guess):
+        if len(guess) > 1:
+            raise InvalidGuessedLetterException
+        
+        if guess in self.answer:
+            new_string = ''
+            for index,char in enumerate(self.answer):
+                if char.lower() == guess.lower():
+                    new_string += char
+                else:
+                    new_string += self.masked[index]
+            self.masked = new_string    
+            return GuessAttempt(guess, hit=True)
+        
+"""
+for i in range(len(self.answer):
+    masked_char = self.masked[i]
+    answer_char = self.answer[i]
+    if answer_char.lower() == guess.lower():
+        new_string += answer_char
+    else:
+        new_string += masked_char
+self.masked = new_string
 
+for answer_char, masked_char in zip(self.answer, self.masked):
+    
+    if answer_char.lower() == guess.lower():
+        new_string += answer_char
+    else:
+        new_string += masked_char
+self.masked = new_string
+"""
 
 class HangmanGame(object):
     pass
