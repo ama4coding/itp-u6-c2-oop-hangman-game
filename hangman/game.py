6a49e57fc7a178d41807a5de292f683a072a14a4
Andre <andre4coding@gmail.com>
2018-08-04 17:12:55 +0000

    Guess attempt working

--- hangman/game.py
+++ hangman/game.py
@@ -2,7 +2,23 @@ from .exceptions import *
 
 
 class GuessAttempt(object):
-    pass
+    def __init__(self, letter, hit=None, miss=None):
+        self.letter = letter
+        self.hit = hit
+        self.miss = miss
+        
+        if hit and miss:
+            raise InvalidGuessAttempt
+        
+    def is_hit(self):
+        if self.hit:
+            return True
+        return False
+        
+    def is_miss(self):
+        if self.miss:
+            return True
+        return False
 
 
 class GuessWord(object):
